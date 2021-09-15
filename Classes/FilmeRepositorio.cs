8339400aba3da71acefdc26be927e7b8965952f7
klebiaklfernandes <klebiakol@gmail.com>
2021-09-15 02:29:12 -0300

    commit inicial

---
+++ Classes/FilmeRepositorio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeriesDIO.Classes
+{
+    public class FilmeRepositorio
+    {
+		private List<Filme> listaFilme = new List<Filme>();
+		public void Atualiza(int id, Filme objeto)
+		{
+			listaFilme[id] = objeto;
+		}
+
+		public void Exclui(int id)
+		{
+			listaFilme[id].Excluir();
+		}
+
+		public void Insere(Filme objeto)
+		{
+			listaFilme.Add(objeto);
+		}
+
+		public List<Filme> Lista()
+		{
+			return listaFilme;
+		}
+
+		public int ProximoId()
+		{
+			return listaFilme.Count;
+		}
+
+		public Filme RetornaPorId(int id)
+		{
+			return listaFilme[id];
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+## Projeto Criando Um APP  Simples de Cadastro de Séries em .NET :computer:##
+
+
+
+A partir desse projeto, foi adicionado um novo CRUD de filmes. Com isso trouxe a possibilidade de navegar entre os 2 menus, o de séries e de filmes.A partir desse projeto, foi adicionado um novo CRUD de filmes.:film_projector:
+
